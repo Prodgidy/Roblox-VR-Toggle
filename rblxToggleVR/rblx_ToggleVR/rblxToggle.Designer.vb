256b5ba205953ebf8186fa711b63a72955262801
Funymeam <59900313+Prodgidy@users.noreply.github.com>
2024-05-26 11:48:18 +1000

    Source Code
    Source code for the tool
    You can look through this to make sure im not tryna rat you

---
+++ rblxToggleVR/rblx_ToggleVR/rblxToggle.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class vrToggler
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(vrToggler))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnToggle = New System.Windows.Forms.Button()
+        Me.lblVRStatus = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(899, 450)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'btnToggle
+        '
+        Me.btnToggle.Font = New System.Drawing.Font("Comic Sans MS", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnToggle.Location = New System.Drawing.Point(271, 258)
+        Me.btnToggle.Name = "btnToggle"
+        Me.btnToggle.Size = New System.Drawing.Size(259, 80)
+        Me.btnToggle.TabIndex = 1
+        Me.btnToggle.Text = "TOGGLE VR"
+        Me.btnToggle.UseVisualStyleBackColor = True
+        '
+        'lblVRStatus
+        '
+        Me.lblVRStatus.AutoSize = True
+        Me.lblVRStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblVRStatus.Font = New System.Drawing.Font("Comic Sans MS", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblVRStatus.Location = New System.Drawing.Point(271, 345)
+        Me.lblVRStatus.Name = "lblVRStatus"
+        Me.lblVRStatus.Size = New System.Drawing.Size(176, 29)
+        Me.lblVRStatus.TabIndex = 2
+        Me.lblVRStatus.Text = "VR is currently:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(783, 430)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "made by: @funymeam"
+        '
+        'vrToggler
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(896, 445)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblVRStatus)
+        Me.Controls.Add(Me.btnToggle)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "vrToggler"
+        Me.Text = "RBLX VR Toggler"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnToggle As Button
+    Friend WithEvents lblVRStatus As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ rblxToggleVR/rblx_ToggleVR/rblxToggle.vb
@@ -0,0 +1,52 @@
+﻿'Made by the funyman
+'Date of creation was 25/may/2024 at around 11:58pm i think
+
+Imports System.IO 'very important
+Public Class vrToggler
+    ' Declares public variables
+    Dim rblxSettingsLocation As String
+    Dim settingLine As Integer
+    Dim vrActive As Boolean
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Sets the roblox settings location on startup and runs a vr state check / set
+        rblxSettingsLocation = "C:\Users\" & SystemInformation.UserName & "\AppData\Local\Roblox\GlobalBasicSettings_13.xml"
+        vrState()
+    End Sub
+    Private Sub btnToggle_Click(sender As Object, e As EventArgs) Handles btnToggle.Click
+        Dim lines() As String = IO.File.ReadAllLines(rblxSettingsLocation) ' Creates essentially a digging turtle that digs through every line of a given file (in this case the settings folder)
+        If vrActive = True Then
+            'Checks to see if its enabled and if it is then it will get every line of code and alter the line with the VREnabled setting and then runs the vr check again.
+
+            lines(settingLine) = "			<bool name=" & """" & "VREnabled" & """" & ">false</bool>"
+            IO.File.WriteAllLines(rblxSettingsLocation, lines)
+            vrState()
+        ElseIf vrActive = False Then
+            'Checks to see if its disabled and if it is then it will have the opposite effect expect for the vr check
+
+            lines(settingLine) = "			<bool name=" & """" & "VREnabled" & """" & ">true</bool>"
+            IO.File.WriteAllLines(rblxSettingsLocation, lines)
+            vrState()
+        End If
+    End Sub
+
+    'The code that runs only when its called and will get the line the VREnabled setting it is on, check if VREnabled is true and setis the VRStatus
+    Sub vrState()
+        Dim lines() As String = File.ReadAllLines(rblxSettingsLocation) ' This variable is used to read lines AMOUNT to target the VREnabled setting
+        Dim reader As StreamReader = File.OpenText(rblxSettingsLocation) ' This variable is used to read what the line CONTAINS to check if VREnabled = True
+
+        'This code counts the lines and goes through all of them 1 by 1, the -1 is used because the first line of a text file is line(0) and the lines.Length counts it as 1
+        For n = 0 To lines.Length - 1
+            Dim line As String = reader.ReadLine
+            If line = "			<bool name=" & """" & "VREnabled" & """" & ">true</bool>" Then
+                settingLine = n
+                vrActive = True
+                lblVRStatus.Text = "VR is currently: Enabled"
+            ElseIf line = "			<bool name=" & """" & "VREnabled" & """" & ">false</bool>" Then
+                settingLine = n
+                vrActive = False
+                lblVRStatus.Text = "VR is currently: Disabled"
+            End If
+        Next
+        reader.Close() ' Closes the reader because 
+    End Sub
+End Class
